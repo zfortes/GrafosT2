d945efd6d579da7a22edbbba0ec33acd2e8a79c2
saulo <saulomf91@hotmail.com>
2018-06-01 01:11:30 -0300

    Insercao de testes na main

--- Project03/arquivo.cpp
+++ Project03/arquivo.cpp
@@ -14,86 +14,70 @@ using namespace std;
 Grafo *Arquivo::lerGrafo(){
     ifstream file ("entradaProj3TAG.txt");
 
-	int i, j, ab=0, ae=0;
-	string aux, sae;
+	int j, cont=1;
+	string aux;
+	string sae;
 	VerticeP Vprof;
 	VerticeE Vescola;
 	Grafo *grafo=NULL;
-
 	if (file.is_open()){
 	        string line;
 	        getline (file,line);
 	        grafo = new Grafo(100, 50);
-	        while (! file.eof() ){
+	        while (file.eof()==false){
 	        	getline (file,line);
-	        	i = 0;
 	        	j=1;
 			//(P6, 2): (E10, E5, E6, E27, E13)
 			if(line[j]=='P'){
 				j++;
 				while(line[j]!=','){
-					aux[i]=line[j];
-        	        		i++;
+					aux+=line[j];
         	        		j++;
         	    		}
 				Vprof.id = stoi(aux);
+				aux="";
 				j=j+2;
-				aux[0] = line[j];
-				Vprof.habilitacoes = stoi(aux);
+				sae = line[j];
+				Vprof.habilitacoes = stoi(sae);
 				j=j+2;
 				while(line[j]!=')'){
 					if(line[j]=='E'){
-						i=0;
 						j++;
 						while((line[j]!=',') && (line[j]!=')')){
-							aux[i]=line[j];
-							i++;
+							aux+=line[j];
 							j++;
 						}
 						Vprof.preferencia.push_back(stoi(aux));
+						aux="";
 					}
 					if(line[j]!=')')
 						j++;
 				}
-				grafo->professores[ab].push_back(Vprof);
-				ab++;
+				grafo->professores->push_back(Vprof);
+				Vprof.preferencia.clear();
 			}//(E36):(2):(1)
 			else if(line[j]=='E'){
 				j++;
 				while(line[j]!=')'){
-					aux[i]=line[j];
-					i++;
+					aux+=line[j];
 					j++;
 				}
 				Vescola.id = stoi(aux);
+				aux="";
 				while(line[j]!='(')
 					j++;
 				j++;
-				aux[0]=line[j];
-				Vescola.preferencia = stoi(aux);
+				sae=line[j];
+				Vescola.preferencia = stoi(sae);
 				while(line[j]!='(')
 					j++;
 				j++;
-				aux[0]=line[j];
-				Vescola.vagas = stoi(aux);
-				grafo->escolas[ae].push_back(Vescola);
-				ae++;
+				sae=line[j];
+				Vescola.vagas = stoi(sae);
+				grafo->escolas->push_back(Vescola);
 			}
-/*            while (line[i]!=' '){
-                aux[j]=line[i];
-                i++;
-                j++;
-            }
-            ab = stoi(aux);
-            i++;
-            j=0;
-            while (line[i]!='\0'){
-                aux[j]=line[i];
-                i++;
-                j++;
-            }
-            ae = stoi(aux);
-            grafo->add_adjacente(ab, ae);*/
+			cont++;
+			if(cont>=155) break;
         }
         file.close();
     }
--- Project03/main.cpp
+++ Project03/main.cpp
@@ -9,19 +9,36 @@
 using namespace std;
 
 int main(){
-	int id, hab, pf=2;
+	int id, hab, professor, escola;
+	list<VerticeP>::iterator prof;
+	list<VerticeE>::iterator esc;
+	list<int>::iterator pref;
 	Arquivo *arquivo = new Arquivo();
 	Grafo *grafo = arquivo->lerGrafo();
 
-	if (grafo->professores.size() > pf){
-		list<VerticeP>::iterator it = next(grafo->professores.begin(), pf);
-		cout << it.id << endl;
-		cout << it.habilitacoes << endl;
-	}
-	//Vprof.preferencia.push_back(stoi(aux));
-	//grafo->professores[ab].push_back(Vprof);
-//	cout << id << endl;
-//	cout << hab << endl;
 	
+	srand (time(NULL));
+	professor = rand() % 100 + 1;
+	escola = rand() % 50 + 1;
+	cout << "P " << professor << endl;
+	cout << "E " << escola << endl;
+	if (grafo->professores->size() >= professor){
+		prof = next(grafo->professores->begin(), professor-1);
+		cout << "professor:" << prof->id;
+		cout << " habilitacoes:" << prof->habilitacoes;
+		cout << " lista preferencia:";
+		for(int i : prof->preferencia){
+			cout << i << " ";
+		}
+		cout << endl;	
+	}
+
+	if (grafo->escolas->size() >= escola){
+		esc = next(grafo->escolas->begin(), escola-1);
+		cout << "escola:" << esc->id;
+		cout << " exigencia:" << esc->preferencia;
+		cout << " vagas:" << esc->vagas << endl;
+	}
+
 	return 0;
 }
